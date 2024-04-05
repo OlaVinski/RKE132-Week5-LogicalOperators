682fe73c41bdeb89a707122715682cf4f7b217b0
Olavi Raigla <oraigla@tktk.ee>
2024-04-05 14:10:55 +0300

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+//AND - &&
+//nö range tingimuste kontroll
+//true && true --> true
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+//OR - || (pipes) - AltGr+"> <" nupp
+
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
